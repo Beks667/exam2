5668f81cc55513476c4c47a9c679670cf812b9b7
Ваше Имя <you@example.com>
2021-05-15 21:26:39 +0600

    beks

--- task_4.py
+++ task_4.py
@@ -1,14 +1,14 @@
-# def func(*args):
-#     result = 0
-#     for i in args:
-#         try:
-#             result+=i
-#         except TypeError:
-#             continue
-#     return result
+def func(*args):
+    result = 0
+    for i in args:
+        try:
+            result+=i
+        except TypeError:
+            continue
+    return result
 
-# a=func(1,2,3,5)
-# print(a)
+a=func(1,2,3,5)
+print(a)
 
 def func_2(*args):
     result=0
